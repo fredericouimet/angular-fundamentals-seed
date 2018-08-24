4584eb1c36756fd3be36bb1263ef9b26773c6e2d
Ouimet, Frederic <frederic.ouimet@sap.com>
2018-08-24 17:35:45 -0400

    03-bootstrapping

--- main.ts
+++ main.ts
@@ -3,3 +3,4 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 
 platformBrowserDynamic().bootstrapModule(AppModule);
+// Commit 03: the whole content of the file
