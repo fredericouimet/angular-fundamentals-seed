76e2d7169e0cb008edf0ff239656fb28f2aadef2
Ouimet, Frederic <frederic.ouimet@sap.com>
2018-08-27 14:32:55 -0400

    15-feature-modules

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+
+import { PassengerDashboardModule } from './passenger-dashboard/passenger-dashboard.module';
 
 import { AppComponent } from './app.component';
 
@@ -11,7 +12,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    CommonModule
+    CommonModule,
+    PassengerDashboardModule
   ],
   bootstrap: [AppComponent]
 })
