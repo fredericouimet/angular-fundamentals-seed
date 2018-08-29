9e5bfee6719b303b6870d39c5e1c8e90f5467d05
Ouimet, Frederic <frederic.ouimet@sap.com>
2018-08-29 15:24:28 -0400

    37.5 using ngFor (with previous commit as well)

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 
+interface Nav {
+  link: string,
+  name: string,
+  exact: boolean
+}
+
 @Component({
   selector: 'app-root',
   styleUrls: ['app.component.scss'],
@@ -18,4 +24,22 @@ import { Component } from '@angular/core';
     </div>
   `
 })
-export class AppComponent {}
+export class AppComponent {
+  nav: Nav[] = [
+    {
+      link: '/',
+      name: 'Home',
+      exact: true
+    },
+    {
+      link: '/passengers',
+      name: 'Passengers',
+      exact: true
+    },
+    {
+      link: '/oops',
+      name: '404',
+      exact: false
+    }
+  ];
+}
